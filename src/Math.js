d14b0576644fa53a60b2a9ac6fd4cd6be2feda3f
Steven <svramirez20@gmail.com>
2024-02-11 00:32:06 -0500

    add rows & numbers

--- src/Math.js
+++ src/Math.js
@@ -2,6 +2,26 @@ import { useState } from "react";
 
 function Math() {
 
+    return (
+        <div className="main">
+            <div className="row1"></div>
+           <div className="row2"></div>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+            <div className="row3">
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+            </div>
+            <div className="row4">
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+            </div>
+            <div className="row5"></div>
+        </div>
+    );
 }
 
 export default Math;
